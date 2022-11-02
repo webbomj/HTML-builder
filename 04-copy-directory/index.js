22bc3da2a5250b2eeada0dab5a62d044058aad6c
Andrey Lapin <nethack@mail.ru>
2022-11-02 14:58:43 +0400

    feat: add logic to 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+const fsPromises = require('fs/promises')
 
+const newFolder = path.resolve(__dirname, 'files-copy')
+const dir = path.resolve(__dirname, 'files')
+
+function copyDir() {
+  fsPromises.mkdir(newFolder, {recursive: true}, err => {
+    if(err) throw err
+  });
+
+  fs.readdir(dir, (err, allDir) => {
+    allDir.forEach((el) => {
+      const filePath = path.resolve(__dirname, 'files', el)
+      const newFilePath = path.resolve(newFolder, el)
+      console.log(filePath, el)
+      fs.copyFile(filePath, newFilePath, (err) => {
+        if (err) throw err
+      } )
+    })
+  })
+
+
+}
+
+copyDir()
